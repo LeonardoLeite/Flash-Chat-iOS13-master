177ec129e4710db460ba46fd1294a5062317f136
Leonardo Bezerra Leite <lableite@gmail.com>
2020-11-24 17:33:28 -0300

    Initial commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Leonardo A B Leite on 21/11/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
